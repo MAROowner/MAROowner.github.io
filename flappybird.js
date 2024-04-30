f3c99136d73fa97e18f5b294d23e9cfaa02aeffc
MAROowner <130857926+MAROowner@users.noreply.github.com>
2024-04-30 22:41:23 +0300

    Update flappybird.js

--- flappybird.js
+++ flappybird.js
@@ -1,6 +1,6 @@
 let board = document.getElementById("board");
-let boardWidth = board.width;
-let boardHeight = board.height;
+let boardWidth = 380;
+let boardHeight = 590;
 let context;
 
 let birdWidth = 34;
@@ -18,12 +18,25 @@ let bird = {
 
 window.onload = function () {
 	context = board.getContext("2d");
+	board.height = boardHeight;
+	board.width = boardWidth;
 
-	birdX = birdHeight / 8;
-	birdY = birdWidth / 2;
+	birdX = boardWidth / 8;
+	birdY = boardHeight / 2;
 	bird.x = birdX;
 	bird.y = birdY;
 
-	context.fillStyle = "green";
-	context.fillRect(bird.x, bird.y, bird.width, bird.height);
+	birdImg = new Image();
+	birdImg.src = "Image/flappybird.png";
+	birdImg.onload = function(){
+		context.drawImage(birdImg, bird.x, bird.y, bird.width, bird.height);
+	}
+
+	requestAnimationFrame(update);
+}
+
+function update(){
+	requestAnimationFrame(update);
+	context.clearRect(0, 0, board.width, board.height);
+	context.drawImage(birdImg, bird.x, bird.y, bird.width, bird.height);
 }
