9c6b883f4bdab28159cda79d8e3ce11445170f5e
MAROowner <130857926+MAROowner@users.noreply.github.com>
2024-04-30 22:22:48 +0300

    Update flappybird.js

--- flappybird.js
+++ flappybird.js
@@ -1,11 +1,29 @@
-let board;
-//let boardWidth = 380;
-//let boardHeight = 590;
+let board = document.getElementById("board");
+let boardWidth = board.width;
+let boardHeight = board.height;
 let context;
 
+let birdWidth = 34;
+let birdHeight = 24;
+
+let birdX;
+let birdY;
+
+let bird = {
+	x: 0,
+	y: 0,
+	width: birdWidth,
+	height: birdHeight
+}
+
 window.onload = function () {
-	board = document.getElementById("board");
-	//board.height = boardHeight;
-	//board.width = boardWidth;
 	context = board.getContext("2d");
+
+	birdX = birdHeight / 8;
+	birdY = birdWidth / 2;
+	bird.x = birdX;
+	bird.y = birdY;
+
+	context.fillStyle = "green";
+	context.fillRect(bird.x, bird.y, bird.width, bird.height);
 }
