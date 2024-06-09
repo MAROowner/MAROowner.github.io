4c66c0cbbedbf4217709f21c9109500887fd887c
MAROowner <130857926+MAROowner@users.noreply.github.com>
2024-06-09 14:32:54 +0300

    Update db_connect.php

--- db_connect.php
+++ db_connect.php
@@ -1,21 +1,30 @@
 <?php
 require_once 'config.php';
 
-$name = $_POST['name'];
-$points = $_POST['points'];
+function addRecord($name, $points)
+{
+	global $servername, $username, $password, $dbname;
 
-$conn = new mysqli($servername, $username, $password, $dbname);
+	$conn = new mysqli($servername, $username, $password, $dbname);
 
-if ($conn->connect_error) {
-	die("Помилка підключення: " . $conn->connect_error);
+	if ($conn->connect_error) {
+		return json_encode(array('success' => false, 'message' => "Помилка підключення: " . $conn->connect_error));
+	}
+
+	$sql = "INSERT INTO users (username, points) VALUES ('$name', $points)";
+
+	if ($conn->query($sql) === TRUE) {
+		$response = json_encode(array('success' => true, 'message' => "Новий запис успішно додано до бази даних"));
+	} else {
+		$response = json_encode(array('success' => false, 'message' => "Помилка: " . $sql . "<br>" . $conn->error));
+	}
+
+	$conn->close();
+	return $response;
 }
 
-$sql = "INSERT INTO users (username, points) VALUES ('$name', $points)";
-
-if ($conn->query($sql) === TRUE) {
-    echo "Новий запис успішно додано до бази даних";
-} else {
-    echo "Помилка: " . $sql . "<br>" . $conn->error;
+if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+	$name = $_POST['name'];
+	$points = $_POST['points'];
+	echo addRecord($name, $points);
 }
-
-$conn->close();
